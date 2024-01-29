0cea3567b8c7612815aef342b6d02e2787525152
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-01-29 10:04:13 +0530

    text themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop_02/utils/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      themeMode: ThemeMode.system,
+      theme: CAppTheme.lightTheme,
+      darkTheme: CAppTheme.darkTheme,
       home: const Home(),
     );
   }
---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop_02/utils/theme/text_theme.dart';
+
+class CAppTheme {
+  CAppTheme._();
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor:Colors.white,
+    textTheme:CTextTheme.lightTextTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: CTextTheme.darkTextTheme,
+  );
+}
